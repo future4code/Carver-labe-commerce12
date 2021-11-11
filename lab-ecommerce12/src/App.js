d29142b93204856e8cb11f7d185fd461d86d8e85
evelynoliv <evelynolives@gmail.com>
2021-11-11 20:25:20 -0300

    Template 02

--- lab-ecommerce12/src/App.js
+++ lab-ecommerce12/src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const ContainerFiltros = styled.div`
+border: 2px solid black;
+
+`
+const Titulo = styled.h1`
+color: red;
+
+`
+
+// const Paragrafo = styled.p`
+// `
+
+
+
+export class App extends Component {
+  state = { //presente durante a execucao do app, qd atualiza a pag, muda
+    inputValorMaximo: "",     
+    inputValorMinimo: "", 
+    inputProduto: "",    
+          
+    
+  };
+
+  onChangeMaximo = (event) => {
+    this.setState({ inputValorMaximo: event.target.value }); //modifica o state
+  };
+
+  onChangeMinimo = (event) => {
+    this.setState({ inputaValorMinimo: event.target.value });
+  };
+
+  onChangeProduto = (event) => {
+    this.setState({ inputProduto: event.target.value });
+  };
+
+  filtrarItensMax = (event) => {
+    this.setState({inputValorMaximo: event.target.value})
+ };
+  
+
+  filtrarItensMin = (event) => {
+    this.setState({inputaValorMinimo: event.target.value})
+  };
+
+  
+
+
+ 
+
+  render() {  //o que aparece na tela, renderizado
+
+
+       // const produtosFitrados = this.state.produtos.filter((produto) => {
+    //   return // condicionais
+    // })
+
+    // const listaDeProdutosFiltrados = this.state.produtos.map((produto) => {
+    //   return(
+    //     <Produto
+    //       imagem = {produto.imagem}
+    //       titulo = {produto.titulo}
+    //       preco = {produto.preco}
+    //     />
+    //   )
+    // })
+  
+    
+
+    return (
+
+      <ContainerFiltros>
+        <Titulo>Filtros</Titulo>
+        <p> Valor Mínimo</p>
+        <input min="100" type="number"
+          value={this.state.inputValorMaximo}
+          onChange={this.onChangeMaximo}
+          placeholder="Pesquisa"
+        />
+        <p> Valor Máximo</p>
+        <input max="1000" type="Number"
+          value={this.state.inputaValorMinimo}
+          onChange={this.onChangeMinimo}
+          placeholder="Pesquisa"
+        />
+        <p> Buscar Produtos</p>
+        <input
+          defaultValue=""
+          type="text"
+          ref={this.state.inputProduto} 
+          onChange={this.onChangeProduto}
+          placeholder="Produto"
+          />
+
+        {
+         
+       } 
+   
+        
+      </ContainerFiltros>
+           
+      
+
+      
+    );
+
+    
+
+  
+
+
+
+}
+
+
+
+  
+  
 }
 
 export default App;
+
